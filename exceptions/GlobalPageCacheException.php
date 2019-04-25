73fb5af98353d83d8b5ace9a820e676f185d7481
Saiat Kalbiev <kalbievich11@gmail.com>
2019-04-25 15:01:49 +0400

    Add GlobalPageCacheException

---
+++ exceptions/GlobalPageCacheException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sai
+ * Date: 4/25/19
+ * Time: 2:25 PM
+ * @author Saiat Kalbiev <kalbievich11@gmail.com>
+ */
+
+namespace apollo11\yii2GlobalBehaviours\exceptions;
+
+
+class GlobalPageCacheException extends \Exception
+{
+
+}
